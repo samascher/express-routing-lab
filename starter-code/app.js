8fad81078d579f5246f99842cd3bf7732e3ded6b
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-07-20 16:20:17 -0600

    got the methods set up of GET POST UPDATE etc

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,22 @@
 //Did you use npm install to
 //add all these packages
 //to our project?
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const app = express();
 
 // How do we 'require' the candyRouter file?
-var candyRouter;
+
+const candyRouter = require('./candyRouter.js');
 
 app.use(bodyParser.json());
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+
+app.use('/api', candyRouter);
 
 app.listen(3000);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 router = express.Router();
-
-var candies;
+const candies;
 
 //What would need to go into candies
 //in order to pass our first test?
 
+//INDEX
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+	res.send('You made a GET request to the / path AKA the INDEX motherfucker');
 });
 
-// Fill out the rest of the routes here
+//NEW
+router.get('/new', function(req,res) {
+	res.send('You made a GET request to the /new path...aka NEW');
+});
+
+//EDIT
+router.get('/:id/edit', function(req,res) {
+	res.send('You made a GET resquest to the /:id/edit path aka the EDIT');
+});
+
+//SHOW
+router.get('/:id', function(req, res) {
+	res.send('You made a GET request to the /:id path aka SHOW');
+});
+
+//CREATE
+router.post('/', function(req, rest){
+	res.send('You made a POST reuqest to the / path aka CREATE');
+});
+
+//UPDATE
+router.put('/:id', function(req, res){
+	res.send('You made teh PUT request in the /:id path ... aka UPDATE motherfucker!!');
+});
+
+//DESTROY
+router.delete('/:id', function(req, res) {
+	res.send('You made a delete request to the /:id path AKA destroy!!!!');
+});
 
 module.exports = router;
